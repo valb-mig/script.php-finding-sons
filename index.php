41d792e3694747724896b5d19ec576ab03549a02
valb.mig <valb.mig@gmail.com>
2024-10-14 10:34:23 -0300

    :bug:(fix) Ajustando relatório

--- index.php
+++ index.php
@@ -3,14 +3,21 @@
 class Grafo 
 {
     private $url_z1;
+    private $resumePath;
+    private $outputDir;
 
     public function __construct() 
     {
-        $this->url_z1 = "/var/www/html/levier";
+        $this->outputDir  = "./dist";
+        $this->resumePath = $this->outputDir."/!README.md";
+        $this->url_z1     = "/var/www/html/levier";
     }
 
     public function run() 
     {
+        touch($this->resumePath);
+        file_put_contents($this->resumePath, "| File | Lines |\n| ------ | ------ |\n", FILE_APPEND);
+
         $this->scanDir($this->url_z1);
     }
 
@@ -28,7 +35,8 @@ class Grafo
                     $this->scanDir($path);
                 }
                 else if (preg_match('/\.(php|inc)$/', $file)) {
-                    $this->add($file, $dir);
+                    $lineCount = $this->countLinesInFile($path);
+                    $this->add($file, $dir, $lineCount);
                 }
             }
         }
@@ -36,15 +44,40 @@ class Grafo
         closedir($handle);
     }
 
-    public function add(string $file, string $dir) 
+    private function countLinesInFile(string $path)
     {
-        $outputDir = "./dist";
-        
+        $lineCount = 0;
+
+        if (is_file($path)) {
+
+            $fileHandle = fopen($path, "r");
+    
+            if ($fileHandle) {
+
+                while (!feof($fileHandle)) {
+                    fgets($fileHandle);
+                    $lineCount++;
+                }
+    
+                fclose($fileHandle);
+            }
+        }
+    
+        return $lineCount;
+    }
+
+    public function add(string $file, string $dir, string $lines) 
+    {
         $fileName      = pathinfo($file, PATHINFO_FILENAME);
         $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
-        $currentFile   = "$outputDir/$fileName.$fileExtension.md";
+        $currentFile   = "$this->outputDir/$fileName.$fileExtension.md";
+
+        # Count lines
+
+        file_put_contents($this->resumePath, "| [[$fileName.$fileExtension]] | $lines |\n", FILE_APPEND);
 
         # Criando arquivo atual
+        
         touch($currentFile);
 
         $pattern = '/\b(include|include_once|require|require_once)\s*\(?[\'"]([^\'"]+)[\'"]\)?/i';
@@ -54,13 +87,13 @@ class Grafo
         echo "Arquivo processado: $dir/$file\n";
 
         # verificando se o arquivo contém algum include / require
-
+        
         if (preg_match_all($pattern, $fileContent, $matches)) {
 
             foreach ($matches[2] as $includedFile) {
 
                 $includedFileName = basename($includedFile);
-                echo "Include encontrado: [$includedFileName]\n";
+                echo "Include found: [$includedFileName]\n";
 
                 # Incluindo arquivos que são vinculados no arquivo percorrido atual
                 file_put_contents($currentFile, "[[$includedFileName]]\n", FILE_APPEND);
